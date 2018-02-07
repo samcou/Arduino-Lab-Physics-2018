dc52529e3cebc0a1ab6b9d77fc0583e9392043b1
samcou <36229747+samcou@users.noreply.github.com>
2018-02-07 18:41:05 +0000

    Update _5_sec_blink.ino

--- _5_sec_blink.ino
+++ _5_sec_blink.ino
@@ -1,11 +1,11 @@
 int pin = 13; //defining pin as pin 13
-void setup() {
-  pinMode(pin, OUTPUT);
+void setup() { // Begin setup
+  pinMode(pin, OUTPUT); //setting pin 13 to output
 }
-void loop() {
-  digitalWrite(pin, HIGH);
-  delay(1000);
-  for (int i=0; i<5; i++) {
+void loop() { // Begin Loop
+  digitalWrite(pin, HIGH); //LED turns on
+  delay(1000); //1 second delay
+  for (int i=0; i<5; i++) { //Light blinks for 5 rounds then continues
     digitalWrite (pin, HIGH);
     delay (250);
     digitalWrite (pin, LOW);
