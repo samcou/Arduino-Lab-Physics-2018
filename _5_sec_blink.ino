5da8aa5f3fda84b5d3733adcece554948968f71d
samcou <36229747+samcou@users.noreply.github.com>
2018-02-07 18:35:34 +0000

    Add files via upload

---
+++ _5_sec_blink.ino
@@ -0,0 +1,16 @@
+int pin = 13; //defining pin as pin 13
+void setup() {
+  pinMode(pin, OUTPUT);
+}
+void loop() {
+  digitalWrite(pin, HIGH);
+  delay(1000);
+  for (int i=0; i<5; i++) {
+    digitalWrite (pin, HIGH);
+    delay (250);
+    digitalWrite (pin, LOW);
+    delay (250);
+  }
+  digitalWrite(pin, LOW);
+  delay(1000);
+}
